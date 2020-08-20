049a44aa7efbed870cdea869d6c753bb30cf11dd
matieuruth <matieu.ruth@gmail.com>
2020-08-19 21:38:30 -0400

    squares slice of state

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -14,7 +14,7 @@ Only one square (or none) can be active at any given point.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 const listOfSquareIds = ['sqA', 'sqB', 'sqC', 'sqD'];
@@ -23,6 +23,8 @@ export default function Squares() {
   // Use the state hook twice, as we need two slices of state: 'squares' and
   // 'activeSquare'. One holds the _array_ of square ids, and the other keeps track
   // of the currently active square (if any).
+  const [squares, setSquares] = useState(listOfSquareIds);
+  const [activeSquare, setActiveSquare] = useState(true);
 
   const getClassName = id => {
     // This is NOT a click handler but a helper, used inside the JSX (see below).
